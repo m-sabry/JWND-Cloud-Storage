ebd073395a692cae05780235501b974decc7ab56
msabry <msabry12345>
2020-11-20 19:22:58 +0200

    Added security stuff

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfiguration.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfiguration.java
@@ -1,6 +1,27 @@
 package com.udacity.jwdnd.course1.cloudstorage.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    public void configure(HttpSecurity http) throws Exception{
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/home", "/static/**",
+                        "/css/**", "/js/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
 }

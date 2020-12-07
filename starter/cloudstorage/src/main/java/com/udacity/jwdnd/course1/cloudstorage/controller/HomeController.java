f1f04b82e7ff5b3141ca7ea791a0e864d1737104
msabry <msabry12345>
2020-12-07 19:37:40 +0200

    Loading notes, files and credentials

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.service.CredentialService;
+import com.udacity.jwdnd.course1.cloudstorage.service.FileService;
 import com.udacity.jwdnd.course1.cloudstorage.service.NoteService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +12,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @Autowired NoteService noteService;
+    private final NoteService noteService;
+    private final FileService fileService;
+    private final CredentialService credentialService;
+
+    public HomeController(NoteService noteService, FileService fileService, CredentialService credentialService) {
+        this.noteService = noteService;
+        this.fileService = fileService;
+        this.credentialService = credentialService;
+    }
+
     @GetMapping("/")
-    public String home(Model model){
-        model.addAttribute("notes", noteService.getAll());
+    public String home(Authentication authentication, Model model){
+        User currentUser = (User) authentication.getPrincipal();
+
+        model.addAttribute("notes", noteService.getUserNotes(currentUser.getUserId()));
+        model.addAttribute("files", fileService.getUserFiles(currentUser.getUserId()));
+        model.addAttribute("credentials", credentialService.getUserCredentials(currentUser.getUserId()));
         return "home";
     }
 }
